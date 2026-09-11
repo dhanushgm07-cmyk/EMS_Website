4a098c89bd2257fd273d3f154431c7b24e0aee14
dhanushgm07-cmyk <dhanushgm07@gmail.com>
2026-09-11 17:15:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1629,9 +1629,17 @@ if (
 mins < 60
 ) {
 
-return (`${mins}m ago`);
+return (
+  `${mins}m ago`
+);
+
 }
-return `${Math.floor(mins / 60)}h ago`;
+
+return (
+${Math.floor(
+      mins / 60
+    )}h ago
+);
 }
 
 function buildAlerts() {
